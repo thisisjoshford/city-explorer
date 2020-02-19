ba6ffaa206225bcbaa055e68c1e78eda67c13b8e
Josh Ford <thisisjoshford@gmail.com>
2020-02-19 11:57:18 -0800

    its working!!!!! got location data to show up!

--- app.js
+++ app.js
@@ -3,32 +3,26 @@ const express = require('express');
 //get the json data and put that in geoData
 const geoData = require('./geo.json');
 //tells this app to run express!
-
-const weather = require('./darksky.json')
+const weather = require('./darksky.json');
 
 const app = express();
-
-console.log(geoData);
-
-app.get('*', (req, res) => {
-    res.json({
-        ohNo: '404'
-
-    });
-});
 
 //initialize the global state of late and long so its accessable in other routes
 let lat;
 let lng;
 
+// make sure your routes are in ORDER!!!!  ie 404 at the end
+
 app.get('/location', (request, respond) => {
     //look at the query params and location
     const location = request.query.search; 
-    //
+    //will use when we actually hit api
+    console.log('using location...', location);
+
     const cityData = geoData.results[0];
     //update the global state of lat and long so it is accessible in other routes
     lat = cityData.geometry.location.lat;
-    long = cityData.geometry.location.lng;
+    lng = cityData.geometry.location.lng;
    
     respond.json({
         formated_query: cityData.formatted_address,
@@ -38,15 +32,19 @@ app.get('/location', (request, respond) => {
     });
 });
 
-app.get('/weather', (req, res){
+app.get('/weather', (req, res) => {
     //use the lat and long from earlier to get weather data for the selected area
     const portlandWeather = getWeatherData(/*lat*lng*/);
 
     //respond with json in the appropriate form
     res.json(portlandWeather);
-})
+});
+app.get('*', (req, res) => {
+    res.send('404 error... you done goofed...');
+});
 
-const getWeatherData = (lat,long) => {
+
+const getWeatherData = (lat, lng) => {
     return weather.daily.data.map(forecast => {
         return {
             forecast: forecast.summary,
