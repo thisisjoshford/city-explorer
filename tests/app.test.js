7a5daf5ec50b93623c98bf629221504b23693125
Josh Ford <thisisjoshford@gmail.com>
2020-02-20 10:38:01 -0800

    weather test passing

--- tests/app.test.js
+++ tests/app.test.js
@@ -20,4 +20,50 @@ describe('testing all paths', () => {
             // the callback has a 'done' that we can call to fix stuff :sparkle-emoji:
             done();
         });
+    test('weather path',
+    // get the done function to call after the test
+        async(done) => {
+            // feed our express app to the supertest request
+            const response = await request(app)
+                // and hit out express app's about route with a /GET
+                .get('/weather/');
+            // check to see if the response is what we expect
+            expect(response.body).toEqual([{
+                forecast: expect.any(String),
+                time: expect.any(String),
+            },
+            {
+                forecast: expect.any(String),
+                time: expect.any(String),
+            },
+            {
+                forecast: expect.any(String),
+                time: expect.any(String),
+            },
+            {
+                forecast: expect.any(String),
+                time: expect.any(String),
+            },
+            {
+                forecast: expect.any(String),
+                time: expect.any(String),
+            },
+            {
+                forecast: expect.any(String),
+                time: expect.any(String),
+            },
+            {
+                forecast: expect.any(String),
+                time: expect.any(String),
+            },
+            {
+                forecast: expect.any(String),
+                time: expect.any(String),
+            },
+            ]);
+            // it should have a status of 200
+            expect(response.statusCode).toBe(200);
+            // the callback has a 'done' that we can call to fix stuff :sparkle-emoji:
+            done();
+        });
 });
